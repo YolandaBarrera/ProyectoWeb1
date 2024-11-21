ba685408e81b9232380d528848fd125f94468a75
YolandaBarrera <136383160+YolandaBarrera@users.noreply.github.com>
2024-11-21 14:57:56 -0600

    Update conexion.php

--- Proyecto/logica/conexion.php
+++ Proyecto/logica/conexion.php
@@ -4,6 +4,12 @@ $user_db = "root";
 $pass_db = "BigQnevadas5@";
 $db_name = "datos";
 
+// Conexión a Infininy free:
+// $host_db = "sql205.infinityfree.com";
+// $user_db = "if0_37329524";
+// $pass_db = "u9IqlPoJdeTjM";
+// $db_name = "if0_37329524_datos";
+
 $conexion = new mysqli($host_db,$user_db,$pass_db,$db_name);
 
 if($conexion->connect_error){
